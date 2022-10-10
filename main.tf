7821c037cb637de534fc8b4354b9ed7797df4a34
jadashi97 <njadauniversal23@yahoo.com>
2022-10-10 14:32:00 -0500

    remove sensitive infor

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
 provider "aws" {
-  region     = "us-west-2"
-  access_key = "AKIATNIAYYAG7NNSFZXG"
-  secret_key = "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"
+  region = "us-west-2"
+}
+
+resource "aws_instance" "my-first-server" {
+  ami           = "ami-08c40ec9ead489470"
+  instance_type = "t2.micro"
 }
