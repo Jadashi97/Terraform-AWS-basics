27980a1ef70ef1bfcdaf2a214fa518ffd25d9ce7
jadashi97 <njadauniversal23@yahoo.com>
2022-10-10 15:31:19 -0500

    add more config files

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
+
 }
 
 resource "aws_instance" "my-first-server" {
   ami           = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
+  tags = {
+    Name = "ubuntu"
+  }
 }
